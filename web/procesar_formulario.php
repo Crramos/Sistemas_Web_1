9c44099977768661e1ac7c59fe53a5bfa0333b61
Crramos <carlos.ramosrial@usp.ceu.es>
2023-11-10 16:16:40 +0100

    formulario hecho, falta pulirlo y el metodo de pago

---
+++ web/procesar_formulario.php
@@ -0,0 +1,5 @@
+<?php
+// Redirigir al usuario a la página "mytickets.html"
+header("Location: mytickets.html");
+exit(); // Asegura que no se ejecuten más instrucciones después de la redirección
+?>
