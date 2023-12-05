51b4a854346b6da1ddb799976942b07ca642a0a9
Ana Fraile <ana.frailecaparros@usp.ceu.es>
2023-12-05 17:55:51 +0100

    Añadido modulo events (no implementado en views)

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,7 @@
 const database = {};
 
 database.user = require('./models/user.model');
+database.event = require('./models/event.model');
 
 function initializeUsers(){
     const NAMES = ["carlos", "juan pablo", "miguel", "ana"];
@@ -9,8 +10,16 @@ function initializeUsers(){
     });
 }
 
+function initializeEvents(){
+    const NAMES = ["a", "b", "c", "d"];
+    NAMES.forEach(function(name){
+        database.event.register(name, 20, "Un evento", null);
+    });
+}
+
 function initializeDB(){
     initializeUsers();
+    initializeEvents();
 }
 
 initializeDB();
