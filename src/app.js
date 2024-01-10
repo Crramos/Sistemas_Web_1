89969d7f896b6bac5db49307eb8d74fc8cb99764
Juan Pablo Zurita <juanpablo.zuritasoto@usp.ceu.es>
2024-01-10 19:20:10 +0100

    Logout añadido

--- src/app.js
+++ src/app.js
@@ -66,7 +66,10 @@ app.use('/profile', profileRouter);
 app.use('/editProfile', editProfileRouter);
 app.use('/myTickets', myticketsRouter);
 app.use('/entrada_evento', entrada_eventoRouter);
-
+app.use('/logout', (req,res) =>{
+  req.session.destroy();
+  res.redirect("/");
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
