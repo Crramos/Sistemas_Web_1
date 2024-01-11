ee331914ed8e81ae5f523eaba3362cd9be7ca3f6
Juan Pablo Zurita <juanpablo.zuritasoto@usp.ceu.es>
2024-01-11 01:35:51 +0100

    CSS modificado del chat.

--- src/public/js/chat.js
+++ src/public/js/chat.js
@@ -7,29 +7,28 @@ const username = chatContainer.getAttribute("data-username");
 
 socket.on("loadMessages", (previousMessages) => {
     previousMessages.forEach((msg) => {
-        const item = document.createElement("li");
-        item.textContent = `${msg.username}: ${msg.message}`;
-        
-        if (msg.username === username) {
-            item.classList.add("current-user-message");
-        } else {
-            item.classList.add("other-user-message");
-        }
-
-        messages.appendChild(item);
+        displayMessage(msg);
     });
 });
 
-
 form.addEventListener('submit', function(e){
     e.preventDefault();
-    if(input.value){
-        socket.emit("chat", { message: input.value, username: username});
+    const messageText = input.value.trim();
+
+    if (messageText) {
+        const mentionedUser = extractMentionedUser(messageText);
+
+        socket.emit("chat", { message: messageText, username: username, mentionedUser: mentionedUser });
         input.value = "";
     }
 });
 
-socket.on("chat", (data) =>{
+socket.on("chat", (data) => {
+    displayMessage(data);
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+});
+
+function displayMessage(data) {
     const item = document.createElement("li");
     item.textContent = `${data.username}: ${data.message}`;
 
@@ -40,5 +39,12 @@ socket.on("chat", (data) =>{
     }
 
     messages.appendChild(item);
-    chatContainer.scrollTop = chatContainer.scrollHeight;
-});
+}
+
+function extractMentionedUser(messageText) {
+    // Obtener el nombre antes del @
+    const mentionedUser = messageText.split('@')[0].trim();
+
+    // Devolver solo el nombre de usuario o null si no se encuentra ningún @ en el mensaje
+    return mentionedUser !== messageText ? mentionedUser : null;
+}
